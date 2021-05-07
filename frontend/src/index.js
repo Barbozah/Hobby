7e2d014bbc1f42313285058028f84a01d202247f
Antonio Barboza <34846614+Barbozah@users.noreply.github.com>
2021-05-07 15:13:23 -0300

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { AppContainer } from 'react-hot-loader'
-import App from './App'
+import App from './app'
 
 const renderApp = (NextApp) => {
   render(
