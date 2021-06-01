cf853e7c8f4bcde1e53ef8dd4c8e51d510941a64
igor-felipe <54617151+igor-felipe@users.noreply.github.com>
2021-05-31 23:00:24 -0300

    hobby-welcame

--- backend/src/middlewares/middleware.js
+++ backend/src/middlewares/middleware.js
@@ -1,14 +1,23 @@
 require('dotenv').config();
 
 exports.middlewareGlobal = (req, res, next) => {
-    res.locals.umaVariaveLocal = 'valor';
+    console.log("________________________Up________________________")
     next();
 }
 
-exports.middlewareGlobal = (req, res, next) => {
-    console.log("______________________________________________________")
-    next();
+exports.hobby = (req, res, next) => {
+  // https://patorjk.com/software/taag/#p=display&f=ANSI%20Shadow&t=Hobby
+    console.log(`
+    ██╗  ██╗ ██████╗ ██████╗ ██████╗ ██╗   ██╗
+    ██║  ██║██╔═══██╗██╔══██╗██╔══██╗╚██╗ ██╔╝
+    ███████║██║   ██║██████╔╝██████╔╝ ╚████╔╝ 
+    ██╔══██║██║   ██║██╔══██╗██╔══██╗  ╚██╔╝  
+    ██║  ██║╚██████╔╝██████╔╝██████╔╝   ██║   
+    ╚═╝  ╚═╝ ╚═════╝ ╚═════╝ ╚═════╝    ╚═╝   
+                                                   
+    `)
 }
+
 
 exports.checkCsrfError = (err, req, res, next) => {
     if(err && 'EBADCSRFTOKEN' === err.code){
