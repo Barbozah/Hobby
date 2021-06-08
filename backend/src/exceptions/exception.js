aaaa21f5ca8525645b7033d6e8ed5e0a4f753e09
igor-felipe <54617151+igor-felipe@users.noreply.github.com>
2021-06-08 00:50:16 -0300

    Update exception.js

--- backend/src/exceptions/exception.js
+++ backend/src/exceptions/exception.js
@@ -8,23 +8,34 @@ const InvalidCredentials = require('./errors/invalid-credentials');
 const ResourceNotFound = require('./errors/resource-not-found');
 
 const getKnownError = (error) => {
-  let currentError = error;
-  if (!currentError || !currentError.name) { currentError = { name: 'InternalServerError' }; }
 
-  const knownErrors = {
-    JsonWebTokenError: new Unauthorized(),
-    TokenExpiredError: new InvalidSession(),
-    ValidationError: new InvalidFields(error),
-    InternalServerError: new InternalServerError(),
-    AlreadyExists: new AlreadyExists(currentError.message),
-    InvalidCredentials: new InvalidCredentials(error.message),
-    ResourceNotFound: new ResourceNotFound(),
-    Unauthorized: new Unauthorized(),
-    InvalidSession: new InvalidSession(),
-    InvalidFields: new InvalidFields(),
-  };
-  return knownErrors[currentError.name] || error;
-};
+  switch (error.name) {
+
+    case 'UnauthorizedError': error = new Unauthorized()
+      break;
+
+    case 'TokenExpiredError': error = new InvalidSession()
+      break;
+
+    case 'ValidationError': error = new InvalidFields(error.message)
+      break;
+
+    case 'Unauthorized':
+    case 'InvalidSession':
+    case 'InvalidFields':
+    case 'InternalServerError':
+    case 'AlreadyExists':
+    case 'ResourceNotFound':
+    case 'InvalidCredentials':
+      break;
+    default:
+      error = new InternalServerError();
+      break;
+  }
+
+return error;
+
+}
 
 module.exports = {
   getKnownError,
