98fd28c0099b3edc6a07282a756ebf2629ad0de6
rodrigoguedes98 <diguinhu_ro@hotmail.com>
2021-06-30 03:14:40 -0300

    Adaptando api de enviar email para criação pedido

--- backend/src/controllers/orderController.js
+++ backend/src/controllers/orderController.js
@@ -28,9 +28,9 @@ module.exports.findAllByUserId = async (req, res, next) => {
         const { query: { page, size, user_id } } = req;
 
         const orders = await orderSchema.find({ user_id: user_id })
-        .select('-__v')
-        .skip((page || 0) * (size || 10))
-        .lean();
+            .select('-__v')
+            .skip((page || 0) * (size || 10))
+            .lean();
 
         if (!orders) { throw new ResourceNotFound("Nenhum pedido encontrado."); }
 
@@ -74,6 +74,8 @@ module.exports.create = async (req, res, next) => {
         };
 
         order = await order.save();
+        
+        sendEmail(order._id);
 
         res.json({
             message: "Pedido cadastrado com sucesso.",
@@ -106,95 +108,102 @@ module.exports.updateStatus = async (req, res, next) => {
         order = await orderSchema.useFindAndModify(
             { _id: _id },
             { $push: { status: status } },
-            { new: true, select: '-__v', runValidators: true}
+            { new: true, select: '-__v', runValidators: true }
         );
-
+        
         res.json(order);
 
     } catch (error) {
         next(error);
     }
 };
-module.exports.payment = async(req, res, next) => {
 
-    try {   
-        
-        let { body: { _id } } = req;
+async function sendEmail(_id) {
 
-        const order = await orderSchema.findOne({ _id });
-        
-        if (!order) { throw new ResourceNotFound("Pedido não encontrado."); }
-        
-        //Validate if last status its waiting
-        let lastStatus = order.status[order.status.length - 1].name;
-        if(!(lastStatus === 'waiting')){ throw new InvalidPaymentRequest("Pedido com status " + lastStatus)};
+    const order = await orderSchema.findOne({ _id });
 
-        //Valdate if user exists
-        const user = await UserSchema.findOne({_id: order.user_id});
-        if (!user || !user.status) { throw new ResourceNotFound("Usuário não encontrado."); }
-        
-        // //Get games name
-        let itemList = order.itemList.map( (item) => item.game_id );
-        const games = await GameSchema.find({ '_id': { $in: itemList } });
+    if (!order) { throw new ResourceNotFound("Pedido não encontrado."); }
 
-        let gameData = await games.map((game) => ({
-            game_id: game.id,
-            title: game.title,
-            price: game.price,
-            discount: game.discount
-        }));
-        
-        sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-        const from = process.env.SENDGRID_SENDER_EMAIL;
-        let subject = 'Confirmação de pagamento pedido ' + _id;
-        
-        //Init body
-        let html = '<div>' +
-                        '<h1>Prezado aventureiro ' + user.name + '</h1>' + 
-                        '<p>Aqui estão os jogos do seu pedido:</p>' +
-                + '</div>';
+    //Validate if last status its waiting
+    let lastStatus = order.status[order.status.length - 1].name;
+    if (!(lastStatus === 'waiting')) { throw new InvalidPaymentRequest("Pedido com status " + lastStatus) };
 
-        html += '<table style="width:100%">' + 
-                        '<tr style="text-align: left">' +
-                            '<th style="border: 1px solid black">Item</th>' + 
-                            '<th style="border: 1px solid black">Preço</th>' +
-                            '<th style="border: 1px solid black">Desconto</th>' +
-                        '</tr>';
-                 
-        for(let count = 0; count < gameData.length; count++){
-            html += '<tr>' +
-                        '<td style="border: 1px solid black">' + gameData[count].title + '</td>' +
-                        '<td style="border: 1px solid black">' + gameData[count].price + '</td>' +
-                        '<td style="border: 1px solid black">' + gameData[count].discount + '</td>' +
-                    '</tr>';
-        }        
-        
+    //Valdate if user exists
+    const user = await UserSchema.findOne({ _id: order.user_id });
+    if (!user || !user.status) { throw new ResourceNotFound("Usuário não encontrado."); }
+
+    // //Get games name
+    let itemList = order.itemList.map((item) => item.game_id);
+    const games = await GameSchema.find({ '_id': { $in: itemList } });
+
+    let gameData = await games.map((game) => ({
+        game_id: game.id,
+        title: game.title,
+        price: game.price,
+        discount: game.discount
+    }));
+
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const from = process.env.SENDGRID_SENDER_EMAIL;
+    let subject = 'Confirmação de pagamento pedido ' + _id;
+
+    //Init body
+    let html = '<div>' +
+        '<h1>Prezado aventureiro ' + user.name + '</h1>' +
+        '<p>Aqui estão os jogos do seu pedido:</p>' +
+        + '</div>';
+
+    html += '<table style="width:100%">' +
+        '<tr style="text-align: left">' +
+        '<th style="border: 1px solid black">Item</th>' +
+        '<th style="border: 1px solid black">Preço</th>' +
+        '<th style="border: 1px solid black">Desconto</th>' +
+        '</tr>';
+
+    for (let count = 0; count < gameData.length; count++) {
         html += '<tr>' +
-                        '<th>Valor Total</th>' +
-                        '<td><strong>' + order.amount + '</strong></td>' +
-                    '</tr>' +
-            '</table>';
-        //End Body
+            '<td style="border: 1px solid black">' + gameData[count].title + '</td>' +
+            '<td style="border: 1px solid black">' + gameData[count].price + '</td>' +
+            '<td style="border: 1px solid black">' + gameData[count].discount + '</td>' +
+            '</tr>';
+    }
 
-        const msg = {
-            to: user.email,   
-            from, 
-            subject,
-            html
-        }
+    html += '<tr>' +
+        '<th>Valor Total</th>' +
+        '<td><strong>' + order.amount + '</strong></td>' +
+        '</tr>' +
+        '</table>';
+    //End Body
 
-        sgMail
+    const msg = {
+        to: user.email,
+        from,
+        subject,
+        html
+    }
+
+    sgMail
         .send(msg)
         .then(() => {
             console.log('Email enviado com sucesso.')
-            res.json({
-                message: "Email enviado com sucesso."
-            });
         })
         .catch((error) => {
             console.error(error)
             next(error);
         });
+}
+
+module.exports.payment = async (req, res, next) => {
+
+    try {
+
+        let { body: { _id } } = req;
+        
+        sendEmail(_id);
+        res.json({
+            message: "Email enviado com sucesso."
+        });
+
     } catch (error) {
         next(error);
     }
